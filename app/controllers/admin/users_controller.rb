b69fba00d9f3d12ce166aa59102de9f5cb5db18d
jessica sossou <merveille20sossou@gmail.com>
2021-10-20 21:34:27 +0100

    step4 somes correction

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -24,7 +24,10 @@ class Admin::UsersController < ApplicationController
             @user = User.find(params[:id])
             if @user.update(user_params) 
                 redirect_to admin_users_path
+            else
+                render :edit
             end
+
         end
         def show
             @user = User.find(params[:id])
